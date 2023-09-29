e3c432e80d0a4d3a3d7236d60144b6c3364c03dc
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-09-29 18:32:10 +0300

    Amend function for uploading images to server

--- utils/uploadPhotoToServer.js
+++ utils/uploadPhotoToServer.js
@@ -4,7 +4,7 @@ import { storage } from "../firebase/config";
 export const uploadPhotoToServer = async (directory, photoURI, userId) => {
   const response = await fetch(photoURI);
   const file = await response.blob();
-  const photoId = `${userId}_${Date.now().toString()}`;
+  const photoId = directory === "userPhoto" ? `${userId}` : `${userId}_${Date.now().toString()}`;
   const storageRef = ref(storage, `${directory}/${photoId}`);
 
   await uploadBytes(storageRef, file);
