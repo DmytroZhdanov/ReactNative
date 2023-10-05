e58b0efcf29f309d0e17a4e2213d90d5a162d5f4
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-10-05 16:56:48 +0300

    Add warning ignor for "Sending `onAnimatedValueUpdate` with no listeners registered."

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import { LogBox } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
 import Main from "./components/Main/Main";
 
 export default function App() {
+  LogBox.ignoreLogs(["Sending `onAnimatedValueUpdate` with no listeners registered."]);
+
   return (
     <Provider store={store}>
       <Main />
